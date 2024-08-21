c8e7f73ed567e24cd2ad9d1d67cfa3377c5957a6
Ivan Zheng <104228630+IvanZh19@users.noreply.github.com>
2024-08-21 19:01:23 -0400

    Add get_moves and tweak classes and highlighting

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class White(pygame.sprite.Sprite):
         self.pos = pos
         self.x = pos[0]
         self.y = pos[1]
+        self.color = 'white'
         self.rect = self.image.get_rect(center=pos_to_coords(pos))
 
 class Black(pygame.sprite.Sprite):
@@ -24,6 +25,9 @@ class Black(pygame.sprite.Sprite):
         self.image = pygame.image.load('graphics/black.png').convert_alpha()
         self.image = pygame.transform.rotozoom(self.image,0,1/4)
         self.pos = pos
+        self.x = pos[0]
+        self.y = pos[1]
+        self.color = 'black'
         self.rect = self.image.get_rect(center=pos_to_coords(pos))
 
 class Highlight(pygame.sprite.Sprite):
@@ -43,12 +47,67 @@ def reset_board():
         black.add(Black((7,i)))
         black.add(Black((0,i)))
 
-def show_moves(piece,piece_group):
-    # first collect the pieces that are on all lines that the piece is on
-    ns_list = [p for p in piece_group if p.x == piece.x]
-    we_list = [p for p in piece_group if p.y == piece.y]
-    nw_se_list = [p for p in piece_group if p.x - piece.x == piece.y - p.y]
-    ne_sw_list = [p for p in piece_group if p.x - piece.x == p.y - piece.y]
+def valid_move_y(t_piece, t_list, t_len, opp_color, inc):
+    '''Helper for get_moves that checks valid moves based on y conditions'''
+    add_move = True
+    if inc:
+        for p in t_list:
+            if (t_piece.y + t_len > 7) or (p.color == opp_color and p.y < t_piece.y + t_len) or (p.color != opp_color and p.y == t_piece.y + t_len):
+                add_move = False
+                break
+    else:
+        for p in t_list:
+            if (t_piece.y - t_len < 0) or (p.color == opp_color and p.y > t_piece.y - t_len) or (p.color != opp_color and p.y == t_piece.y - t_len):
+                add_move = False
+                break
+    return add_move
+
+def valid_move_x(t_piece, t_list, t_len, opp_color, inc):
+    '''Helper for get_moves that checks valid moves based on x conditions'''
+    add_move = True
+    if inc:
+        for p in t_list:
+            if (t_piece.x + t_len > 7) or (p.color == opp_color and p.x < t_piece.x + t_len) or (p.color != opp_color and p.x == t_piece.x + t_len):
+                add_move = False
+                break
+    else:
+        for p in t_list:
+            if (t_piece.x - t_len < 0) or (p.color == opp_color and p.x > t_piece.x - t_len) or (p.color != opp_color and p.x == t_piece.x - t_len):
+                add_move = False
+                break
+    return add_move
+
+def get_moves(piece,piece_list):
+    # first collect the pieces that are on all lines that the piece is on, regardless of color
+    ns_list = [p for p in piece_list if p.x == piece.x]
+    we_list = [p for p in piece_list if p.y == piece.y]
+    nw_se_list = [p for p in piece_list if p.x - piece.x == piece.y - p.y]
+    ne_sw_list = [p for p in piece_list if p.x - piece.x == p.y - piece.y]
+    # define opp_color for checking blocks and captures
+    if piece.color == 'white': opp_color = 'black'
+    else: opp_color = 'white'
+    legal = []
+    ns = len(ns_list)
+    we = len(we_list)
+    nw_se = len(nw_se_list)
+    ne_sw = len(ne_sw_list)
+    if valid_move_y(piece,ns_list,ns,opp_color,inc=True): # n
+        legal.append((piece.x,piece.y+ns))
+    if valid_move_y(piece,ns_list,ns,opp_color,inc=False): # s
+        legal.append((piece.x,piece.y-ns))
+    if valid_move_x(piece,we_list,we,opp_color,inc=True): # e
+        legal.append((piece.x+we,piece.y))
+    if valid_move_x(piece,we_list,we,opp_color,inc=False): # w
+        legal.append((piece.x-we,piece.y))
+    if valid_move_y(piece,nw_se_list,nw_se,opp_color,inc=True): # nw
+        legal.append((piece.x-nw_se,piece.y+nw_se))
+    if valid_move_y(piece,nw_se_list,nw_se,opp_color,inc=False): # se
+        legal.append((piece.x+nw_se,piece.y-nw_se))
+    if valid_move_y(piece,ne_sw_list,ne_sw,opp_color,inc=True): # ne
+        legal.append((piece.x+ne_sw,piece.y+ne_sw))
+    if valid_move_y(piece,ne_sw_list,ne_sw,opp_color,inc=False): # sw
+        legal.append((piece.x-ne_sw,piece.y-ne_sw))
+    return legal
 
 def mouse_to_pos():
     mouse_coords = pygame.mouse.get_pos()
@@ -59,25 +118,25 @@ def mouse_to_pos():
 def check_highlight():
     mouse = pygame.mouse.get_pressed()
     mouse_pos = mouse_to_pos()
-    if mouse[2] and -1 < mouse_pos[0] < 8 and -1 < mouse_pos[1] < 8: # if right clicked on a square
+    # if right clicked on a square, do highlighting
+    if mouse[2] and -1 < mouse_pos[0] < 8 and -1 < mouse_pos[1] < 8:
         select = [h for h in highlight if h.pos == mouse_pos]
         if select == []:
             highlight.add(Highlight((mouse_pos)))
         else:
             select[0].kill()
-    elif mouse[0] and -1 < mouse_pos[0] < 8 and -1 < mouse_pos[1] < 8: # if left clicked on a square
-        hit = False
-        for group in white, black, highlight:
-            for i in group:
-                if i.pos == mouse_pos:
-                    hit = True
-        if not hit:
-            highlight.empty()
-
-
-
-
-
+    # elif left click
+    elif mouse[0]:
+        # check left click inside window
+        mouse_coords = pygame.mouse.get_pos()
+        if mouse_coords[0] <= 1000 and mouse_coords[1] <= 680:
+            hit = False
+            for group in white, black, highlight:
+                for i in group:
+                    if i.pos == mouse_pos:
+                        hit = True
+            if not hit:
+                highlight.empty()
 
 pygame.init()
 screen = pygame.display.set_mode((1000,680))
@@ -93,8 +152,6 @@ border_surf.fill('#432616')
 bg_surf = pygame.Surface((1000,680))
 bg_surf.fill('#757C88')
 
-
-
 white = pygame.sprite.Group()
 black = pygame.sprite.Group()
 highlight = pygame.sprite.Group()
@@ -102,9 +159,9 @@ highlight = pygame.sprite.Group()
 player_color = 'white'
 
 reset_board()
-# white_list = white.sprites()
-# black_list = black.sprites()
-# all_list = white_list + black_list
+white_list = white.sprites()
+black_list = black.sprites()
+all_list = white_list + black_list
 
 while True:
     for event in pygame.event.get():
